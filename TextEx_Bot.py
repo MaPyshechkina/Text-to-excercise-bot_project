a4060895cce3af8703135c45f37ed067acc114e5
MaPyshechkina <149236304+MaPyshechkina@users.noreply.github.com>
2024-10-16 20:08:24 +0300

    Bot.py

--- TextEx_Bot.py
+++ TextEx_Bot.py
@@ -1,36 +1,96 @@
 import telebot
-from telebot import types
 import spacy
-import os
+import random
 
-
+# Загрузка русской модели SpaCy
 nlp = spacy.load("ru_core_news_sm")
 
+# Токен вашего бота в Telegram
+TOKEN = "TOKEN"
 
-bot = telebot.TeleBot("Token")
-print(bot)
+# Инициализация бота
+bot = telebot.TeleBot(TOKEN)
 
+# Переменная для хранения последнего запроса пользователя
+last_request = {}
 
-
-# Функция для нахождения частотных существительных в генитиве
-
-def freq_nouns_genitive(message):
-    text = message.text
+# Функция для лемматизации существительных и добавления их в скобки в предложении
+def bracket_nouns(text):
     doc = nlp(text)
-    cases = {}
-    for token in doc:
-        if token.pos_ != 'PUNCT':
-            cases[token.text] = token.morph.get('Case')
+    result = []
 
-    gen_words = [word for word, cases in cases.items() if 'Gen' in cases]
+    for sent in doc.sents:
+        nouns = [token for token in sent if token.pos_ == "NOUN"]
 
-    return gen_words[:60]
+        if len(nouns) == 1:
+            result.append(sent.text.replace(nouns[0].text, f"({nouns[0].lemma_})"))
+        elif len(nouns) >= 2:
+            lemmatized_nouns = [token.lemma_ for token in nouns[:2]]
+            replaced_sent = sent.text.replace(nouns[0].text, f"({lemmatized_nouns[0]})")
+            replaced_sent = replaced_sent.replace(nouns[1].text, f"({lemmatized_nouns[1]})")
+            result.append(replaced_sent)
+
+    return result
 
 
+# Функция для получения 10 случайных предложений
+def get_random_sentences(sentences):
+    random_sentences = random.sample(sentences, min(len(sentences), 10))
+    numbered_sentences = [(i + 1, sentence) for i, sentence in enumerate(random_sentences)]
+    return numbered_sentences
 
-# Функция для создания упражнения с частотными существительными в предложениях
-def exercise_with_nouns(message):
-    text = message.text
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+    itembtn1 = telebot.types.KeyboardButton('/Упражнение на все падежи')
+    itembtn2 = telebot.types.KeyboardButton('Упражнение на родительный падеж')
+    itembtn3 = telebot.types.KeyboardButton('Упражнение на предложный падеж')
+    itembtn4 = telebot.types.KeyboardButton('Упражнение на дательный падеж')
+    itembtn5 = telebot.types.KeyboardButton('Упражнение на винительный падеж')
+    itembtn6 = telebot.types.KeyboardButton('Упражнение на творительный падеж')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6)
+    bot.send_message(message.chat.id,
+                     "Привет! Я бот, который делает упражнения типа 'раскройте скобки'.",
+                     reply_markup=markup)
+
+
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text == '/Упражнение на все падежи':
+        last_request[message.chat.id] = 'Упражнение на все падежи'
+        bot.send_message(message.chat.id, "Отправьте мне текст для анализа:")
+    elif message.text == 'Упражнение на родительный падеж':
+        last_request[message.chat.id] = 'exercise_with_genitive'
+        bot.send_message(message.chat.id, "Отправьте мне текст для анализа:")
+    elif message.text == 'Упражнение на предложный падеж':
+        last_request[message.chat.id] = 'exercise_with_locative'
+        bot.send_message(message.chat.id, "Отправьте мне текст для анализа:")
+    elif message.text == 'Упражнение на дательный падеж':
+        last_request[message.chat.id] = 'exercise_with_dative'
+        bot.send_message(message.chat.id, "Отправьте мне текст для анализа:")
+    elif message.text == 'Упражнение на винительный падеж':
+        last_request[message.chat.id] = 'exercise_with_accusative'
+        bot.send_message(message.chat.id, "Отправьте мне текст для анализа:")
+    elif message.text == 'Упражнение на творительный падеж':
+        last_request[message.chat.id] = 'exercise_with_instrumental'
+        bot.send_message(message.chat.id, "Отправьте мне текст для анализа:")
+    elif message.chat.id in last_request:
+        # Лемматизация существительных и добавление их в скобки
+        lemmatized_sentences = bracket_nouns(message.text)
+
+        # Получение и отправка 10 случайных предложений
+        random_sentences = get_random_sentences(lemmatized_sentences)
+        bot.send_message(message.chat.id, "10 случайных предложений:")
+        for number, sentence in random_sentences:
+            bot.send_message(message.chat.id, f"{number}. {sentence}")
+        del last_request[message.chat.id]
+
+
+# Функции для выполнения упражнений на падежи
+def exercise_with_genitive(text, chat_id):
     doc = nlp(text)
     cases = {}
     for token in doc:
@@ -46,48 +106,84 @@ def exercise_with_nouns(message):
         else:
             transformed_text += token.text_with_ws
 
-    return transformed_text
+    # Отправка результата обработки
+    bot.send_message(chat_id, transformed_text)
 
-# Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    item1 = types.KeyboardButton('Частотные существительные в Gen')
-    item2 = types.KeyboardButton('Упражнение с частотными существительными в Gen')
-    markup.add(item1, item2)
-    bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
+def exercise_with_locative(text, chat_id):
+    doc = nlp(text)
+    cases = {}
+    for token in doc:
+        if token.pos_ != 'PUNCT':
+            cases[token.text] = token.morph.get('Case')
 
+    loc_words = [word for word, cases in cases.items() if 'Loc' in cases]
 
+    transformed_text = ""
+    for token in doc:
+        if token.pos_ == "NOUN" and token.text in loc_words:
+            transformed_text += f"({token.lemma_}) "
+        else:
+            transformed_text += token.text_with_ws
 
+    # Отправка результата обработки
+    bot.send_message(chat_id, transformed_text)
 
-# Обработчик кнопок
-@bot.message_handler(func=lambda message: True)
-def handle_buttons(message):
-    if message.text == 'Частотные существительные в Gen':
-        bot.send_message(message.chat.id, "Введите текст для анализа:")
-        bot.register_next_step_handler(message, get_genitive_freq_nouns)
-    elif message.text == 'Упражнение с частотными существительными в Gen':
-        bot.send_message(message.chat.id, "Введите текст для анализа:")
-        bot.register_next_step_handler(message, get_exercise_with_nouns)
+def exercise_with_dative(text, chat_id):
+    doc = nlp(text)
+    cases = {}
+    for token in doc:
+        if token.pos_ != 'PUNCT':
+            cases[token.text] = token.morph.get('Case')
 
+    dat_words = [word for word, cases in cases.items() if 'Dat' in cases]
 
-        
+    transformed_text = ""
+    for token in doc:
+        if token.pos_ == "NOUN" and token.text in dat_words:
+            transformed_text += f"({token.lemma_}) "
+        else:
+            transformed_text += token.text_with_ws
 
-# Обработчик текстовых сообщений для кнопки 1
-def get_genitive_freq_nouns(message):
-    gen_words = freq_nouns_genitive(message)
-    bot.send_message(message.chat.id, "Частотные существительные в Gen:\n" + ", ".join(gen_words))
+    # Отправка результата обработки
+    bot.send_message(chat_id, transformed_text)
 
+def exercise_with_accusative(text, chat_id):
+    doc = nlp(text)
+    cases = {}
+    for token in doc:
+        if token.pos_ != 'PUNCT':
+            cases[token.text] = token.morph.get('Case')
 
+    acc_words = [word for word, cases in cases.items() if 'Acc' in cases]
 
+    transformed_text = ""
+    for token in doc:
+        if token.pos_ == "NOUN" and token.text in acc_words:
+            transformed_text += f"({token.lemma_}) "
+        else:
+            transformed_text += token.text_with_ws
 
-# Обработчик текстовых сообщений для кнопки 2
-def get_exercise_with_nouns(message):
-    transformed_text = exercise_with_nouns(message)
-    with open("exercise.txt", "w") as file:
-        file.write(transformed_text)
-    bot.send_document(message.chat.id, open("exercise.txt", "rb"))
-    os.remove("exercise.txt")
+    # Отправка результата обработки
+    bot.send_message(chat_id, transformed_text)
 
+def exercise_with_instrumental(text, chat_id):
+    doc = nlp(text)
+    cases = {}
+    for token in doc:
+        if token.pos_ != 'PUNCT':
+            cases[token.text] = token.morph.get('Case')
 
+    ins_words = [word for word, cases in cases.items() if 'Ins' in cases]
+
+    transformed_text = ""
+    for token in doc:
+        if token.pos_ == "NOUN" and token.text in ins_words:
+            transformed_text += f"({token.lemma_}) "
+        else:
+            transformed_text += token.text_with_ws
+
+    # Отправка результата обработки
+    bot.send_message(chat_id, transformed_text)
+
+# Запуск бота
 bot.polling()
